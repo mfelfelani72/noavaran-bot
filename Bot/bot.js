8b0304b5be6e907263d6e3a760b5858431cd18a9
mohammad <mfelfelani72@gmail.com>
2025-05-09 17:52:55 +0000

    edit

--- Bot/bot.js
+++ Bot/bot.js
@@ -4,22 +4,21 @@ const { Telegraf } = require("telegraf");
 const TOKEN = configures.tokenBot;
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://aimoonhub.imoonex.ir/dashboard/home";
-
+const web_link_aimoonhub = "https://aimoonhub.imoonex.ir/dashboard/home";
+const web_link_imoonex = "http://79.175.177.113:24300/landing";
 
 bot.start((ctx) =>
-  ctx.reply("salam mohammad aziz :)))))", {
+  ctx.reply("welcom to the Noavran bot", {
     reply_markup: {
-      keyboard: 
-      [
+      keyboard: [
         [
-          { text: "select order", web_app: { url: web_link } },
-          { text: "our shop", web_app: { url: "https://mfcloner.am/eshop-blade/public/" } },
+          { text: "Aimoonhub", web_app: { url: web_link_aimoonhub } },
+          { text: "imoonex", web_app: { url: web_link_imoonex } },
         ],
-        [
-          { text: "about me", web_app: { url: "https://mfcloner.am/" } },
-          { text: "our restaurant", web_app: { url: "https://mfcloner.am/restaurant/" } },
-        ],
+        // [
+        //   { text: "about me", web_app: { url: "https://mfcloner.am/" } },
+        //   { text: "our restaurant", web_app: { url: "https://mfcloner.am/restaurant/" } },
+        // ],
       ],
     },
   })
